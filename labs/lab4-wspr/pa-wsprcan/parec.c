b84cfca80afcc2a2868fa47b0403817f1b8311f0
jehad1212 <71128110+jehad1212@users.noreply.github.com>
2022-05-30 13:40:20 +1000

    Update parec.c

--- labs/lab4-wspr/pa-wsprcan/parec.c
+++ labs/lab4-wspr/pa-wsprcan/parec.c
@@ -1,21 +1,4 @@
-/***
-  This file is part of PulseAudio.
 
-  PulseAudio is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2.1 of the License,
-  or (at your option) any later version.
-
-  PulseAudio is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public License
-  along with PulseAudio; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA.
-***/
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -31,20 +14,20 @@
 #include <pulse/error.h>
 #include <pulse/gccmacro.h>
 
-#define	MXRECTIME XXX			// maximum record time in seconds
-#define	SRATE	XXX			// sample rate in Hz
-#define	MXFRAMES (MXRECTIME*SRATE)	// number of frames to read/write
-#define	CHANNELS 1			// number of channels
-#define BYTESPERSAMPLE 2		// 16 bit samples
-#define BUFSIZE (MXFRAMES*CHANNELS*BYTESPERSAMPLE)	// MXRECTIME sec buffer
-#define FNAMESIZE 30			// size of output file name string
+#define MXRECTIME 120                   // maximum record time in seconds
+#define SRATE   12000                     // sample rate in Hz
+#define MXFRAMES (MXRECTIME*SRATE)      // number of frames to read/write
+#define CHANNELS 1                      // number of channels
+#define BYTESPERSAMPLE 2                // 16 bit samples
+#define BUFSIZE (MXFRAMES*CHANNELS*BYTESPERSAMPLE)      // MXRECTIME sec buffer
+#define FNAMESIZE 30                    // size of output file name string
 
 int 
 parec(short *pabuf, int npoints) {
     /* The sample type to use */
-    static const pa_sample_spec ss = 
+    static const ss = 
     {
-        .format = XXX,
+        .format =  PA_SAMPLE_S16LE,
         .rate = SRATE,
         .channels = CHANNELS
     };
